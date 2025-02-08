2fed1023b7bbf1949ee992c4ed563e6a85afd8fc
asrahim88 <tasmimhasan1997@gmail.com>
2025-02-08 15:52:43 +0600

    updated

--- Contact/admin.py
+++ Contact/admin.py
@@ -4,7 +4,7 @@ from . models import SocialInformation,ContactMessage
 # Register your models here.
 
 class SocialInformationAdmin(admin.ModelAdmin):
-    list_display = ["title", "description", "github", "twitter", "linkedin","behance","reddit","dribble","facebook","instagram", "phone_number1","phone_number2","phone_number3", "email1","email2", "address"]
+    list_display = ["title", "description", "github", "twitter", "linkedin","behance","reddit","dribble","facebook","instagram", "whatsApNumber","skype","phone_number", "email1","email2", "address"]
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ["name", "email", "subject","message", "created_at"]
     
